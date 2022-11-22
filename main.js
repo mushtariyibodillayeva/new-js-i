89dea901741a64a060a1b58daf0dd5a05b182d07
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-11-22 21:53:47 +0500

    new code

---
+++ main.js
@@ -0,0 +1,8 @@
+function openDiv() {
+  var theDiv = document.getElementById("myDiv");
+  theDiv.style.transform = "scale(1)";
+}
+function closeDiv() {
+  var closeDiv = document.getElementById("myDiv");
+  closeDiv.style.transform = "scale(0)";
+}
